902cf78cf29915cb87d4c496eb8a40ffdbf7e186
black1277 <stumed@narod.ru>
2021-03-30 20:53:03 +0300

    next work

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", (a)=>{
     const $content = document.getElementById('txt')
+    const $btn1 = document.getElementById('btn1')
+    $btn1.addEventListener('click', ()=>{
+        console.log('btn1 clicked')
+    })
     $content.innerHTML = '<p>Text from js file</p>' + new Date()
 })
