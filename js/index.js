7f61c1154e672a1a312d770ab9f344393159d177
black1277 <stumed@narod.ru>
2021-11-03 22:57:33 +0300

    temp

--- js/index.js
+++ js/index.js
@@ -5,28 +5,29 @@ document.addEventListener("DOMContentLoaded", (a)=>{
     $din.addEventListener('click', (event)=>{
         event.target.classList.toggle('red')
     })
-    $btn1.addEventListener('click', ()=>{
-        console.log('btn1 clicked')
-        animateCSS('din', 'rotateOut').then(()=>{
-            animateCSS('din', 'backInDown')
-        })
+    $btn1.addEventListener('click', async ()=>{
+        TotalLength(98)
+        await  animateCSS('din', 'rotateOut')
+        await  animateCSS('din', 'backInDown')
+        await  delay(1000)
+        await  animateCSS('din', 'backOutDown')
+        await  animateCSS('din', 'fadeInRight')
     })
     $content.innerHTML = '<p>Text from js file</p>' + new Date()
 })
-
+const delay = ms => {
+    return new Promise(r => setTimeout(() => r(), ms))
+}
 const animateCSS = (element, animation, prefix = 'animate__') =>
-    // We create a Promise and return it
+
     new Promise((resolve, reject) => {
-        const animationName = `${prefix}${animation}`;
-        const node = document.getElementById(element);
-        node.classList.add(`${prefix}animated`, animationName);
-
-        // When the animation ends, we clean the classes and resolve the Promise
+        const animationName = `${prefix}${animation}`
+        const node = document.getElementById(element)
+        node.classList.add(`${prefix}animated`, animationName)
         function handleAnimationEnd(event) {
-            event.stopPropagation();
-            resolve('Animation ended');
-            node.classList.remove(`${prefix}animated`, animationName);
+            event.stopPropagation()
+            resolve('Animation ended')
+            node.classList.remove(`${prefix}animated`, animationName)
         }
-
-        node.addEventListener('animationend', handleAnimationEnd, {once: true});
-    });
+        node.addEventListener('animationend', handleAnimationEnd, {once: true})
+    })
